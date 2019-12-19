a709029d80c6fa2dd401afd21a15be40cc8d6e65
anudeepreddy <anudeepc85@gmail.com>
2019-12-19 12:39:25 +0530

    added webpage

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 app.listen(port)
+app.use(express.static('public'))
 app.get('/',(req,res)=>{
-    res.send('Hello World!');
+    res.sendFile('index.html');
 })
